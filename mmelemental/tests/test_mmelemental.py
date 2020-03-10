8f152ab1d857d53d3ca32b7420fe8963af77206d
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-10 04:29:05 -0400

    added test function for gro files

--- mmelemental/tests/test_mmelemental.py
+++ mmelemental/tests/test_mmelemental.py
@@ -1,23 +1,31 @@
 """
 Unit and regression test for the mmelemental package.
 """
-import pytest
+#import pytest
 import sys
 import os
-
+import parmed
 import mmelemental
 from mmelemental.models.util.input import FileInput
 from mmelemental.models.molecule.mm_molecule import MMolecule
 from mmelemental.models.chem.codes import ChemCode
-from mmelemental.components.mm_reader import MMoleculeReader, MMoleculeReaderInput
+from mmelemental.components.molreader_component import MMoleculeReader
+from mmelemental.models.molecule.molreader import MMoleculeReaderInput
 
 def test_mmelemental_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmelemental" in sys.modules
 
-def test_mmelemental_models(debug):
+def test_mmelemental_molgro(debug=True):
+    groFile = FileInput(path=os.path.abspath('mmelemental/data/molecules/dialanine.gro'))
+    topFile = FileInput(path=os.path.abspath('mmelemental/data/molecules/dialanine.top'))
+    
+    top = parmed.gromacs.GromacsTopologyFile(topFile.path)
+
+    return MMolecule.from_file(filename=groFile.path)
+
+def test_mmelemental_molpdb(debug=True):
     pdbFile = FileInput(path=os.path.abspath('mmelemental/data/molecules/dialanine.pdb'))
-    sdfFile = FileInput(path=os.path.abspath('mmelemental/data/molecules/ligand.sdf'))
     smiles = ChemCode(code='CCC')
 
     mol = MMolecule.from_file(filename=pdbFile.path)
@@ -48,6 +56,7 @@ def test_mmelemental_models(debug):
         print('\n')
 
     mol.to_file('tmp.pdb')
+    mol.to_file('tmp.xyz')
 
 test_mmelemental_imported()
-test_mmelemental_models(True)
+test_mmelemental_molpdb(True)
