733785bc1088f7ebf73f207145f60ac8c1a95e50
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-10 04:29:57 -0400

    added utility methods to RDKitMolecule <- ToolkitMolecule

--- mmelemental/models/molecule/rdkit_molecule.py
+++ mmelemental/models/molecule/rdkit_molecule.py
@@ -1,4 +1,7 @@
 from qcelemental import models
+from pydantic import Field
+from typing import List, Dict, Any
+from .gen_molecule import ToolkitMolecule
 
 try:
     from rdkit import rdBase, Chem
@@ -10,11 +13,82 @@ class Bond:
     """ RDKit-based bond order: {0: unspecified, 1: single, etc., up to 21} """  
     orders = list(Chem.BondType.values.values())
 
-class RDKitMolecule(models.ProtoModel):
-    mol: Chem.rdchem.Mol = None
+class RDKitMolecule(ToolkitMolecule):
+    mol: Chem.rdchem.Mol = Field(None, description = 'Rdkit molecule object.')
+        
+    @property
+    def obj_type(self):
+        return 'rdkit'
 
-    class Config:
-        arbitrary_types_allowed = True
+    def gen3D(self, nConformers=1):
+        """ Generates 3D coords for a molecule. Should be called only when instantiating a Molecule object.
+
+        :note: a single unique molecule is assumed. 
+        """
+        rdkmol = Chem.AddHs(self.mol)
+        # create n conformers for molecule
+        confargs = AllChem.EmbedMultipleConfs(rdkmol, nConformers)
+
+        # Energy optimize
+        for confId in confargs:
+            AllChem.UFFOptimizeMolecule(rdkmol, confId=confId)
+
+        return rdkmol
+
+    def remove_residues(self, residues: List[str]) -> Chem.rdchem.Mol:
+        atoms = self._mol.GetAtoms()
+        RWmol = Chem.RWMol(self.mol)
+
+        for atom in atoms:
+            if atom.GetPDBResidueInfo().GetResidueName() in residues:
+                RWmol.RemoveAtom(atom.GetIdx())
+
+        return Chem.Mol(RWmol)
+
+    @classmethod
+    def build_mol(cls, inputs: Dict[str, Any], dtype: str) -> Chem.rdchem.Mol:
+        """ Creates an instance of rdkit.Chem.Mol by parsing an input file (pdb, etc.) or a chemical code (smiles, etc.)
+        """
+        if inputs.file:
+            filename = inputs.file.path
+            if dtype == 'pdb':
+                rdkmol = Chem.MolFromPDBFile(filename, sanitize=False, removeHs=False)
+            elif dtype == 'mol':
+                rdkmol = Chem.MolFromMolFile(filename, sanitize=False, removeHs=False)
+            elif dtype == 'mol2':
+                rdkmol = Chem.MolFromMol2File(filename, sanitize=False, removeHs=False)
+            elif dtype == 'tpl':
+                rdkmol = Chem.MolFromTPLFile(filename, sanitize=False, removeHs=False)
+            elif dtype == 'sdf':
+                rdkmols = Chem.SDMolSupplier(filename, sanitize=False, removeHs=False)
+
+                if len(rdkmols) > 1:
+                    raise ValueError("SDF file should contain a single molecule")
+                else:
+                    rdkmol = rdkmols[0] # should we support multiple molecules?
+
+            else:
+                raise ValueError(f"Unrecognized file type: {ext}")
+
+        # construct RDKit molecule from identifiers
+        elif inputs.code:
+            codeType = inputs.code.codeType
+            function = getattr(Chem, f"MolFrom{codeType}") # should work since validation already done by ChemCode!
+            rdkmol = function(inputs.code.code)
+        else:
+            raise ValueError('Missing input file or code.')
+
+        rdkmol = RDKitMolecule(mol=rdkmol)
+
+        if inputs.code:
+            rdkmol = rdkmol.gen3D()
+
+        # Do cleanup if requested
+        # TODO: must check for existence of residues
+        if inputs.resremove:
+            rdkmol = rdkmol.removeResidues(inputs.resremove)
+
+        return rdkmol
 
 class MMToRDKit:
     @staticmethod
@@ -44,4 +118,4 @@ class MMToRDKit:
             conf.SetAtomPosition(i, coords)
         newmmol.AddConformer(conf)
 
-        return newmmol
+        return newmmol 
