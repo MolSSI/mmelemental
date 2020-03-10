a48361d5126119f7d0c62689398f814c782c9198
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-10 04:26:35 -0400

    renamed mm_constructor to constructor_component

--- mmelemental/components/constructor_component.py
+++ mmelemental/components/constructor_component.py
@@ -6,8 +6,37 @@ from mmelemental.models.molecule.mm_molecule import MMolecule
 from mmelemental.models.chem.codes import ChemCode
 from mmelemental.models.util.input import FileInput
 
-class MMConstructorComponent(GenericComponent):
+class MolConstructorComponent(GenericComponent):
     """ Class for constructing MMolecule from ChemCode or FileInput. """
+
+    @classmethod
+    def output(cls):
+        return MMolecule
+
+    def execute(
+        self,
+        inputs: Dict[str, Any],
+        extra_outfiles: Optional[List[str]] = None,
+        extra_commands: Optional[List[str]] = None,
+        scratch_name: Optional[str] = None,
+        timeout: Optional[int] = None,
+    ) -> Tuple[bool, Dict[str, Any]]:
+
+        return True, self.constructor(inputs)
+
+    def constructor(self, model: ProtoModel) -> MMolecule:
+        if isinstance(model, ChemCode):
+            ctype = str(model.codeType).lower()
+            return MMolecule(symbols=['C'], geometry=[0,0,0], identifiers={ctype: model})
+        elif isinstance(model, FileInput):
+            return MMolecule.from_file(model.path)
+        elif isinstance(model, MMolecule):
+            return model
+        else:
+            raise ValueError(f'Input type {type(model)} not supported for {self.__class__}')
+
+class ForceFieldConstructorComponent(GenericComponent):
+    """ Class for constructing ForceField object from FileInput. """
 
     @classmethod
     def output(cls):
