cae8b84b9e4a231a9560f887b1f8d64f8a98ffb8
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-10 18:41:48 -0400

    input rturns MMoleculeReaderInput

--- mmelemental/components/constructor_component.py
+++ mmelemental/components/constructor_component.py
@@ -3,11 +3,16 @@ from mmcomponents.components.blueprints.generic_component import GenericComponen
 from typing import Any, Dict, List, Optional, Tuple
 
 from mmelemental.models.molecule.mm_molecule import MMolecule
+from mmelemental.models.molecule.molreader import MMoleculeReaderInput
 from mmelemental.models.chem.codes import ChemCode
 from mmelemental.models.util.input import FileInput
 
 class MolConstructorComponent(GenericComponent):
     """ Class for constructing MMolecule from ChemCode or FileInput. """
+
+    @classmethod
+    def input(cls):
+        return MMoleculeReaderInput
 
     @classmethod
     def output(cls):
@@ -26,12 +31,19 @@ class MolConstructorComponent(GenericComponent):
 
     def constructor(self, model: ProtoModel) -> MMolecule:
         if isinstance(model, ChemCode):
-            ctype = str(model.codeType).lower()
+            ctype = str(model.code_type).lower()
             return MMolecule(symbols=['C'], geometry=[0,0,0], identifiers={ctype: model})
         elif isinstance(model, FileInput):
             return MMolecule.from_file(model.path)
         elif isinstance(model, MMolecule):
             return model
+        elif isinstance(model, MMoleculeReaderInput):
+            if model.code:
+                return self.constructor(model.code)
+            elif model.file:
+                return self.constructor(model.file)
+            else:
+                raise ValueError('Input file or chemical code must be supplied.')
         else:
             raise ValueError(f'Input type {type(model)} not supported for {self.__class__}')
 
@@ -55,7 +67,7 @@ class ForceFieldConstructorComponent(GenericComponent):
 
     def constructor(self, model: ProtoModel) -> MMolecule:
         if isinstance(model, ChemCode):
-            ctype = str(model.codeType).lower()
+            ctype = str(model.code_type).lower()
             return MMolecule(symbols=['C'], geometry=[0,0,0], identifiers={ctype: model})
         elif isinstance(model, FileInput):
             return MMolecule.from_file(model.path)
