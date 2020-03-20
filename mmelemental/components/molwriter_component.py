699284e4b3553f5c26d136690aa0f2e4839616d4
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-20 00:17:00 -0400

    removed rdkit import

--- mmelemental/components/molwriter_component.py
+++ mmelemental/components/molwriter_component.py
@@ -1,9 +1,3 @@
-try:
-    from rdkit import rdBase, Chem
-    from rdkit.Chem import AllChem
-except:
-    raise ModuleNotFoundError('Make sure rdkit is installed for code validation.')
-
 from mmcomponents.components.blueprints.generic_component import GenericComponent
 from mmelemental.models.utils.output import FileOutput
 
