9acb32507275b2be806a02f6b84fb31cf924327c
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-25 21:40:31 -0400

    removed resremove Field in MMoleculeReaderInput; added __init__ def

--- mmelemental/models/molecule/mol_reader.py
+++ mmelemental/models/molecule/mol_reader.py
@@ -1,23 +1,38 @@
 from qcelemental.models import ProtoModel
 from mmelemental.models.util.input import FileInput
 from mmelemental.models.chem.codes import ChemCode
-from typing import List, Optional
+from mmelemental.models.molecule.gen_molecule import ToolkitMolecule
+from typing import List, Optional, Union, Dict
 from pydantic import Field
 
 class MMoleculeReaderInput(ProtoModel):
-    file: Optional[FileInput] = Field(
+    file: Optional[Union[FileInput, str]] = Field(
         None, 
         description = 'Input filename object.'
     )
-    code: Optional[ChemCode] = Field(
+    code: Optional[Union[ChemCode, str]] = Field(
         None, 
         description = 'Chemical code object that stores a smiles, smarts, etc. code. See :class: ``Identifiers``.'
-    ) 
-    resremove: Optional[List[str]] = Field(
-        None, 
-        description = 'List of residues (by name) to remove.'
     )
-    dtype: Optional[str] = Field(
+    data: Optional[ToolkitMolecule] = Field(
         None,
-        description = 'Data type specification e.g. rdkit, gro, pdb, etc. See :class: ``MMoleculeReader``.'
+        description = 'Toolkit-specific data object e.g. rdkit.Chem.rdchem.Mol'
+    ) 
+    args: Optional[Dict] = Field(
+        None,
+        description = 'Additional arguments to pass qcelemental molecule.'
     )
+
+    def __init__(self, **args):
+        if (args.get('file') and args.get('code')) or (args.get('file') and args.get('data')) or (args.get('data') and args.get('code')):
+            raise ValueError('Only 1 input Field (code, file, or data) is allowed.')
+
+        if args.get('file'):
+            if isinstance(args['file'], str):
+                args['file'] = FileInput(path=args['file'])
+
+        if args.get('code'):
+            if isinstance(args['code'], str):
+                args['code'] = ChemCode(code=args['code'])
+
+        super().__init__(**args)
