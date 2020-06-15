6aba079769643bec44e9d9822909120f83b98881
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-06-15 12:36:27 -0400

    added extras

---
+++ mmelemental/extras.py
@@ -0,0 +1,18 @@
+from . import _version
+
+__all__ = ["get_information"]
+
+versions = _version.get_versions()
+
+__info = {"version": versions["version"], "git_revision": versions["full-revisionid"]}
+
+
+def get_information(key: str):
+    """
+    Obtains a variety of runtime information about QCElemental.
+    """
+    key = key.lower()
+    if key not in __info:
+        raise KeyError(f"Information key '{key}' not understood.")
+
+    return __info[key]
