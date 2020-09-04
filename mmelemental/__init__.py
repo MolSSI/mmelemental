416725f947b747ffcdda23eb89e8eccaefc5b7d7
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-09-04 17:50:34 -0400

    added import components

--- mmelemental/__init__.py
+++ mmelemental/__init__.py
@@ -5,7 +5,7 @@ A short description of the project.
 
 # Add imports here
 from .mmelemental import *
-from . import models, util
+from . import models, util, components
 
 # Handle versioneer
 from ._version import get_versions
