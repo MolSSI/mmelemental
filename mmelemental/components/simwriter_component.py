ba09942063f55b14d5ebeee9d12155afd6de1486
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-09-17 15:45:01 -0400

    dummy writer works

--- mmelemental/components/simwriter_component.py
+++ mmelemental/components/simwriter_component.py
@@ -1,6 +1,7 @@
 from mmcomponents.components.blueprints.generic_component import GenericComponent
-from mmelemental.models.utils.output import FileOutput
+from mmelemental.models.util.output import FileOutput
 from mmelemental.models.sim.base import Base
+from typing import Dict, List, Any, Optional, Tuple
 
 class SimWriter(GenericComponent):
 
@@ -11,3 +12,19 @@ class SimWriter(GenericComponent):
     @classmethod
     def output(cls):
         return FileOutput
+
+    def execute(
+        self,
+        inputs: Dict[str, Any],
+        extra_outfiles: Optional[List[str]] = None,
+        extra_commands: Optional[List[str]] = None,
+        scratch_name: Optional[str] = None,
+        timeout: Optional[int] = None,
+    ) -> Tuple[bool, Dict[str, Any]]:
+
+    	filename = inputs.filename
+
+    	with open(filename, 'w') as fp:
+    		fp.write('TESTING')
+
+    	return True, FileOutput(path=filename)
