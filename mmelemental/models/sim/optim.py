0e9b83792fb1f0fd9dc502f2057f63dc765d1655
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-09-21 13:41:22 -0400

    replaced Base with SimBase

--- mmelemental/models/sim/optim.py
+++ mmelemental/models/sim/optim.py
@@ -1,7 +1,7 @@
-from .base import Base
+from .base import SimBase
 from mmelemental.models.molecule.mm_molecule import Molecule
 from pydantic import Field
 from typing import Any, Tuple, Union
 
-class Optimization(Base):
+class Optimization(SimBase):
     tol: float = Field(None, description = 'Tolerance used to indicate when the optimization scheme has converd.')
