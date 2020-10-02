1a31a783143c7d78ae1f40444d9756b5644ac5d1
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-10-02 11:13:14 -0400

    added parmed_component

---
+++ mmelemental/components/parmed_component.py
@@ -0,0 +1,145 @@
+from mmcomponents.components.blueprints.generic_component import GenericComponent
+from mmelemental.models.util.output import FileOutput
+from mmelemental.models.molecule.parmed_molecule import ParmedMolecule
+from mmelemental.models.molecule.mol_reader import MoleculeReaderInput
+from mmelemental.models.molecule.mm_molecule import Molecule
+from typing import Dict, Any, List, Tuple, Optional
+
+try:
+    from parmed import structure, topologyobjects
+except:
+    raise ModuleNotFoundError('Make sure parmed is installed.')
+
+class MoleculeToParmed(GenericComponent):
+    """ A model for converting Molecule to ParmEd molecule object. """
+    @classmethod
+    def input(cls):
+        return Molecule
+
+    @classmethod
+    def output(cls):
+        return ParmedMolecule
+
+    def execute(
+        self,
+        inputs: Dict[str, Any],
+        extra_outfiles: Optional[List[str]] = None,
+        extra_commands: Optional[List[str]] = None,
+        scratch_name: Optional[str] = None,
+        timeout: Optional[int] = None,
+    ) -> Tuple[bool, ParmedMolecule]:
+
+        mmol = inputs
+        pmol = structure.Structure()
+
+        for index, symb in enumerate(mmol.symbols):
+
+            name = mmol.names[index]
+            atomic_number = mmol.atomic_numbers[index]
+            charge = mmol.molecular_charge[index]
+            mass = mmol.masses[index]
+
+            # Will likely lose FF-related info ... but then Molecule is not supposed to store any params specific to FFs
+            atom = topologyobjects.Atom(list=None, atomic_number=atomic_number, name=name, type=symb,
+                 charge=charge, mass=mass, nb_idx=0, solvent_radius=0.0,
+                 screen=0.0, tree='BLA', join=0.0, irotat=0.0, occupancy=1.0,
+                 bfactor=0.0, altloc='', number=-1, rmin=None, epsilon=None,
+                 rmin14=None, epsilon14=None)
+
+            resname, resnum = mmol.residues[index]
+
+            pmol.add_atom(atom, resname, resnum, chain='', inscode='', segid='')
+
+        for i, j , btype in mmol.connectivity:
+            atom.bond_to(i, j, Bond.orders[int(btype)])            
+
+        return pmol
+
+    def leftover(self):
+        newmmol = erdkmol.GetMol()
+        conf = Chem.Conformer(len(mmol.geometry))
+        for i, coords in enumerate(mmol.geometry):
+            conf.SetAtomPosition(i, coords)
+        newmmol.AddConformer(conf)
+
+        return True, RDKitMolecule(mol=newmmol)
+
+class ParmedToMolecule(GenericComponent):
+    """ A model for converting ParmEd molecule to Molecule object. """
+
+    @classmethod
+    def input(cls):
+        return MoleculeReaderInput
+
+    @classmethod
+    def output(cls):
+        from mmelemental.models.molecule.mm_molecule import Molecule
+        return Molecule
+
+    def execute(
+        self,
+        inputs: Dict[str, Any],
+        extra_outfiles: Optional[List[str]] = None,
+        extra_commands: Optional[List[str]] = None,
+        scratch_name: Optional[str] = None,
+        timeout: Optional[int] = None) -> Tuple[bool, Dict[str, Any]]:
+        
+        from mmelemental.models.molecule.mm_molecule import Molecule
+
+        if isinstance(inputs, dict):
+            inputs = MoleculeReaderComponent.input()(**inputs)
+        
+        if inputs.data:
+            dtype = inputs.data.obj_type
+            assert dtype == 'parmed'
+            pmol = inputs.data
+        elif inputs.code:
+            raise NotImplementedError('ParmEd does not support instantiating molecule objects from chemical codes.')          
+        elif inputs.file:
+            dtype = inputs.file.ext
+            from mmelemental.models.molecule.parmed_molecule import ParmedMolecule
+            pmol = ParmedMolecule.build_mol(inputs, dtype)
+        else:
+            raise NotImplementedError(f'Data type {dtype} not yet supported.')
+        
+        if inputs.args:
+            orient = inputs.args.get('orient')
+            validate = inputs.args.get('validate')
+            kwargs = inputs.args.get('kwargs')
+        else:
+            orient, validate, kwargs = False, None, None
+
+        symbs = [atom.element_name for atom in pmol.mol.atoms]
+        names = [atom.name for atom in pmol.mol.atoms]
+
+        try:
+            masses = [atom.mass for atom in pmol.mol.atoms]
+        except:
+            masses = None
+
+        try:
+            residues = [(atom.residue.name, atom.residue.idx + 1) for atom in pmol.mol.atoms]
+        except:
+            residues = None
+        
+        connectivity = []
+
+        for bond in pmol.mol.bonds:
+            connectivity.append((bond.atom1.idx, bond.atom2.idx, bond.order))
+
+        geo = pmol.mol.coordinates
+
+        input_dict = {'symbols': symbs, 
+                      'geometry': geo, 
+                      'residues': residues, 
+                      'connectivity': connectivity,
+                      'masses': masses,
+                      'names': names}        
+
+        if kwargs:
+            input_dict.update(kwargs)
+
+        if inputs.code:
+            return True, Molecule(orient=orient, validate=validate, identifiers={dtype: inputs.code}, **input_dict)
+        else:
+            return True, Molecule(orient=orient, validate=validate, **input_dict)
