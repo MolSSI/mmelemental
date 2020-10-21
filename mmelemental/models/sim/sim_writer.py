4cb09ce3cb16e752e7a14499a103139c995bd2fe
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-10-21 00:46:47 -0400

    improved description for filename

--- mmelemental/models/sim/sim_writer.py
+++ mmelemental/models/sim/sim_writer.py
@@ -7,4 +7,5 @@ from typing import Dict, List, Any, Optional, Tuple
 class SimWriterInput(Base):
     model: SimBase = Field(..., description = 'Simulation schema model.')
     engine: Tuple[str, Any] = Field(..., description = 'Name of simulation engine and version: (engine_name, x.x.x).')
-    filename: str = Field(..., description = 'Name of file to be written with the "to_file" method.')
+    filename: str = Field(..., description = 'Name of schema file to be written with the "to_file" method. The base name is \
+    						also used for coordinates, topology, etc. files.')
