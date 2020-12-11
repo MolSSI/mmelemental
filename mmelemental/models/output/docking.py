9700fd94e5ff703a7ea3372cefa863cfcd8e4ef9
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-12-10 21:39:11 -0500

    docking output schema uses SimOutput

--- mmelemental/models/output/docking.py
+++ mmelemental/models/output/docking.py
@@ -1,22 +1,18 @@
 from typing import List, Optional
 from mmelemental.models.input.docking import DockingInput
-from mmelemental.models.molecule.mm_molecule import Molecule
+from mmelemental.models.output.sim import SimOutput
 from mmelemental.models.base import Base
 from pydantic import Field
 
 class DockingOutput(Base):
     dockingInput: DockingInput = Field(..., description="Docking input model.")
-    poses: List[Molecule] = Field(
+    ligand: SimOutput = Field(
         ...,
-        description="Conformation and orientation of the candidate ligand relative to the receptor.",
+        description="Simulation output for the ligand, including its pose and score.",
     )
-    scores: List[float] = Field(
-        ...,
-        description="A metric for evaluating a particular pose. Length of scores must be equal to length of poses.",
-    )
-    flexible: Optional[List[Molecule]] = Field(
+    receptor: Optional[SimOutput] = Field(
         None,
-        description="Conformation and orientation of the flexible side chains in the receptor relative to the ligand.",
+        description="Simulation output for non-rigid receptors i.e. conformation and orientation of the flexible side chains in the receptor relative to the ligand.",
     )
 
 class AffinityOutput(Base):
