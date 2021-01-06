bd8cb8cf10c4865743c4bbca26b1e7780a6dc21e
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-06 12:11:54 -0500

    added basic version of FFReaderInput model

---
+++ mmelemental/models/forcefield/ff_reader.py
@@ -0,0 +1,29 @@
+from mmelemental.models.base import Base
+from mmelemental.models.util.input import FileInput
+from mmelemental.models.molecule.gen_molecule import ToolkitMolecule
+from typing import Optional, Union, Dict
+from pydantic import Field
+
+class FFReaderInput(Base):
+    file: Optional[Union[FileInput, str]] = Field(
+        None, 
+        description = 'Input params/ff file name or object.'
+    )     
+    data: Optional[ToolkitMolecule] = Field(
+        None,
+        description = 'Toolkit-specific data object e.g. rdkit.Chem.rdchem.Mol'
+    ) 
+    args: Optional[Dict] = Field(
+        None,
+        description = 'Additional arguments to pass.'
+    )
+
+    def __init__(self, **args):
+        if args.get('file') and args.get('data'):
+            raise ValueError('Only 1 input Field (file, or data) is allowed.')
+
+        if args.get('file'):
+            if isinstance(args['file'], str):
+                args['file'] = FileInput(path=args['file'])
+
+        super().__init__(**args)
