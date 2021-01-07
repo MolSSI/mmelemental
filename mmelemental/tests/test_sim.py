8b26b29e03eaeb62a5ec9fa38891a25c9a6d59b0
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-06 19:44:28 -0500

    cleaned up code

--- mmelemental/tests/test_sim.py
+++ mmelemental/tests/test_sim.py
@@ -2,9 +2,6 @@
 Simulation test for the mmelemental package.
 """
 #import pytest
-import sys
-import os
-import parmed
 import mmelemental
 from mmelemental.models.util.input import FileInput
 from mmelemental.models.molecule.mm_molecule import Molecule
@@ -14,13 +11,12 @@ from mmelemental.models.input.sim_writer import SimWriterInput
 
 def test_mmelemental_imported():
     """Sample test, will always pass so long as import statement worked"""
+    import sys
     assert "mmelemental" in sys.modules
 
 def test_mmelemental_molgro():
     groFile = FileInput(path='mmelemental/data/molecules/dialanine.gro')
     topFile = FileInput(path='mmelemental/data/molecules/dialanine.top')
-
-    # top = parmed.gromacs.GromacsTopologyFile(topFile.path)
 
     return Molecule.from_file(filename=groFile.path, top_file=topFile)
 
