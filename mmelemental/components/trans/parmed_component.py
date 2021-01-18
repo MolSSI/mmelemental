80765639f1da7c85eb2e2985ca67c5948ea3ee70
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-18 04:02:50 -0500

    added basic version of FF translator

--- mmelemental/components/trans/parmed_component.py
+++ mmelemental/components/trans/parmed_component.py
@@ -3,7 +3,7 @@ from mmelemental.models.molecule.parmed_molecule import ParmedMolecule
 from mmelemental.models.molecule.io_molecule import MolInput
 from mmelemental.models.molecule.mm_molecule import Molecule
 from mmelemental.models.molecule.gen_molecule import ToolkitMolecule
-from mmelemental.models.forcefield.base import ForceField
+from mmelemental.models.forcefield.mm_ff import ForceField
 from mmelemental.models.forcefield.io_ff import FFInput
 from typing import Dict, Any, List, Tuple, Optional
 
@@ -12,7 +12,7 @@ try:
 except:
     raise ModuleNotFoundError('Make sure parmed is installed.')
 
-class MoleculeToParmed(GenericComponent):
+class MolToParmedComponent(GenericComponent):
     """ A component for converting Molecule to ParmEd molecule object. """
     @classmethod
     def input(cls):
@@ -61,7 +61,7 @@ class MoleculeToParmed(GenericComponent):
 
         return True, ParmedMolecule(mol=pmol)
 
-class ParmedToMolecule(GenericComponent):
+class ParmedToMolComponent(GenericComponent):
     """ A component for converting ParmEd molecule to Molecule object. """
 
     @classmethod
@@ -131,12 +131,12 @@ class ParmedToMolecule(GenericComponent):
         else:
             return True, Molecule(orient=orient, validate=validate, **input_dict)
 
-class ParmedToFF(GenericComponent):
-    """ A component for converting ParmEd top object to ForceField (FF) object. """
+class ParmedToFFComponent(GenericComponent):
+    """ A component for converting ParmEd top object to MMSchema ForceField (FF) object. """
 
     @classmethod
     def input(cls):
-        return FFReaderInput
+        return FFInput
 
     @classmethod
     def output(cls):
@@ -153,50 +153,55 @@ class ParmedToFF(GenericComponent):
         if inputs.data:
             dtype = inputs.data.dtype
             assert dtype == 'parmed'
-            pmol = inputs.data
-        elif inputs.code:
-            raise NotImplementedError('ParmEd does not support instantiating molecule objects from chemical codes.')          
+            ff = inputs.data
         elif inputs.file:
             dtype = '.' + inputs.file.ext
-            from mmelemental.models.molecule.parmed_molecule import ParmedMolecule
-            pmol = ParmedMolecule.build(inputs, dtype)
+            from mmelemental.models.forcefield.parmed_ff import ParmedFF
+            ff = ParmedFF.build(inputs, dtype)
         else:
             raise NotImplementedError(f'Data type {dtype} not yet supported.')
-        
+
         if inputs.args:
-            orient = inputs.args.get('orient')
-            validate = inputs.args.get('validate')
-            kwargs = inputs.args.get('kwargs')
-        else:
-            orient, validate, kwargs = False, None, None
+            if 'software' in inputs.args:
+                if inputs.args['software'] == 'gromacs':
+                    if len(ff.ff.itps) > 1:
+                        raise ValueError(f'More than 1 forcefield name detected: {ff.ff.itp}.')
+                    else:
+                        print(f"Forcefield {ff.ff.itps[0].split('/')[0].split('.ff')[0]} detected.")
 
-        symbs = [atom.element_name for atom in pmol.mol.atoms]
-        names = [atom.name for atom in pmol.mol.atoms]
+        data = [(atom.name, atom.charge) for atom in ff.ff.atoms]
+        names, charges = zip(*data)
+        params = [(atom.rmin, atom.rmin_14, atom.epsilon, atom.epsilon_14) for atom in ff.ff.atoms]
 
-        if hasattr(pmol.mol.atoms[0], 'mass'):
-            masses = [atom.mass for atom in pmol.mol.atoms]
+        bonds = [(bond.type.req, bond.type.k) for bond in ff.ff.bonds]
+        bonds_type = [bond.funct for bond in ff.ff.bonds]
 
-        if hasattr(pmol.mol.atoms[0], 'residue'):
-            residues = [(atom.residue.name, atom.residue.idx) for atom in pmol.mol.atoms]
+        angles = [(angle.type.theteq, angle.type.k) for angle in ff.ff.angles]
+        angles_type = [angle.funct for angle in ff.ff.angles]
 
-        connectivity = []
+        # Why the hell does parmed interpret dihedral.type as a list sometimes? See dialanine.top, last 8 dihedral entries. WEIRD!
+        dihedrals = [(dihedral.type[0].phase, dihedral.type[0].phi_k, dihedral.type[0].per, \
+                    dihedral.type[0].scee, dihedral.type[0].scnb) if isinstance(dihedral.type, list) \
+                    else (dihedral.type.phase, dihedral.type.phi_k, dihedral.type.per, \
+                    dihedral.type.scee, dihedral.type.scnb) for dihedral in ff.ff.dihedrals]
+        dihedrals_type = [dihedral.funct for dihedral in ff.ff.dihedrals]
 
-        for bond in pmol.mol.bonds:
-            connectivity.append((bond.atom1.idx, bond.atom2.idx, bond.order))
+        improper_dihedrals = None
+        charge_groups = None
+        exclusions = ff.ff.nrexcl
+        inclusions = None
 
-        geo = pmol.mol.coordinates
+        input_dict = {'bonds': bonds,
+                      'angles': angles,
+                      'dihedrals': dihedrals,
+                      'improper_dihedrals': improper_dihedrals,
+                      'charges': charges,
+                      'charge_groups': charge_groups,
+                      'nonbonded': params,
+                      'exclusions': exclusions,
+                      'inclusions': inclusions,
+                      'types': names, 
+                      'name': 'amber99',
+                    }
 
-        input_dict = {'symbols': symbs, 
-                      'geometry': geo, 
-                      'residues': residues, 
-                      'connectivity': connectivity,
-                      'masses': masses,
-                      'names': names}        
-
-        if kwargs:
-            input_dict.update(kwargs)
-
-        if inputs.code:
-            return True, Molecule(orient=orient, validate=validate, identifiers={dtype: inputs.code}, **input_dict)
-        else:
-            return True, Molecule(orient=orient, validate=validate, **input_dict)
+        return True, ForceField(**input_dict)
