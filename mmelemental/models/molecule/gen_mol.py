744e79ee321cf6314bb8d215449cfc6a310325d2
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-22 15:18:57 -0500

    added many helper methods

--- mmelemental/models/molecule/gen_mol.py
+++ mmelemental/models/molecule/gen_mol.py
@@ -1,17 +1,47 @@
 from mmelemental.models.base import Base
 from pydantic import Field
 from typing import Any
+import importlib
+import inspect
 
 
 class ToolkitMol(Base):
-    mol: Any = Field(..., description="Toolkit-specific molecule object.")
+    """ An abstract base class that acts as a wrapper for toolkit molecules """
+
+    mol: Any = Field(
+        ..., description="Toolkit-specific molecule object."
+    )  # Will be validated during runtime
 
     class Config(Base.Config):
         arbitrary_types_allowed = True
 
     @property
-    def dtype(self):
-        raise NotImplementedError
+    def toolkit(self):
+        return type(self.mol).__module__
+
+    @property
+    def translator(self):
+        name, _ = self.__module__.split(".", 1)
+        return name
+
+    @property
+    def path(self):
+        return self.__module__ + "." + self.__name__
+
+    @property
+    def components(self):
+        comp_mod = importlib.import_module(self.translator + ".components")
+        return inspect.getmembers(comp_mod, inspect.isclass)
+
+    @property
+    def models(self):
+        mod = importlib.import_module(self.translator + ".models")
+        return inspect.getmembers(mod, inspect.isclass)
+
+    def check_type(self):
+        if isinstance(self.mol, self.dtype):
+            return self.mol
+        raise ValidationError
 
     @staticmethod
     def check_name(name) -> str:
