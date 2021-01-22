d8ae3e88105f9d31b29f7ae5a7aa679ca2b4a3da
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-21 19:37:05 -0500

    updated module paths for all translators

--- mmelemental/components/trans/rdkit_component.py
+++ mmelemental/components/trans/rdkit_component.py
@@ -1,33 +1,30 @@
 from mmic.components.blueprints.generic_component import GenericComponent
 from mmelemental.models.util.output import FileOutput
-from mmelemental.models.molecule.io_molecule import MolInput
-from mmelemental.models.molecule.rdkit_molecule import RDKitMolecule
-from mmelemental.models.molecule.gen_molecule import ToolkitMolecule
-from mmelemental.models.molecule.mm_molecule import Molecule
-from mmelemental.models.molecule.rdkit_molecule import Bond
+from mmelemental.models.molecule.io_mol import MolInput
+from mmelemental.models.molecule.rdkit_mol import RDKitMol
+from mmelemental.models.molecule.gen_mol import ToolkitMol
+from mmelemental.models.molecule.mm_mol import Mol
+from mmelemental.models.molecule.rdkit_mol import Bond
 from typing import Dict, Any, List, Tuple, Optional
-from mmelemental.util.decorators import req_rdkit
+from mmelemental.util.decorators import require
 
-try:
-    from rdkit import Chem
-except ImportError:  # pragma: no cover
-    Chem = None  # pragma: no cover
 
 class MolToRDKitComponent(GenericComponent):
     """ A component for converting Molecule to RDKIT molecule object. """
 
-    @req_rdkit
+    @require("rdkit")
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     @classmethod
     def input(cls):
-        return Molecule
+        return Mol
 
     @classmethod
     def output(cls):
-        return RDKitMolecule
+        return RDKitMol
 
+    @require("rdkit")
     def execute(
         self,
         inputs: Dict[str, Any],
@@ -35,7 +32,9 @@ class MolToRDKitComponent(GenericComponent):
         extra_commands: Optional[List[str]] = None,
         scratch_name: Optional[str] = None,
         timeout: Optional[int] = None,
-    ) -> Tuple[bool, RDKitMolecule]:
+    ) -> Tuple[bool, RDKitMol]:
+
+        from rdkit import Chem
 
         rdkmol = Chem.Mol()
         erdkmol = Chem.EditableMol(rdkmol)
@@ -46,7 +45,7 @@ class MolToRDKitComponent(GenericComponent):
             resname, resnum = mmol.residues[index]
             name = mmol.names[index]
 
-            name = ToolkitMolecule.check_name(name)
+            name = ToolkitMol.check_name(name)
 
             residue = Chem.AtomPDBResidueInfo()
             residue.SetResidueName(resname)
@@ -58,7 +57,7 @@ class MolToRDKitComponent(GenericComponent):
             erdkmol.AddAtom(atom)
 
         for i, j, btype in mmol.connectivity:
-            erdkmol.AddBond(i, j, Bond.orders[int(btype)])            
+            erdkmol.AddBond(i, j, Bond.orders[int(btype)])
 
         newmmol = erdkmol.GetMol()
         conf = Chem.Conformer(len(mmol.geometry))
@@ -66,22 +65,22 @@ class MolToRDKitComponent(GenericComponent):
             conf.SetAtomPosition(i, coords)
         newmmol.AddConformer(conf)
 
-        return True, RDKitMolecule(mol=newmmol)
+        return True, RDKitMol(mol=newmmol)
+
 
 class RDKitToMolComponent(GenericComponent):
     """ A model for converting RDKIT molecule to Molecule object. """
 
-    @req_rdkit
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
+
     @classmethod
     def input(cls):
         return MolInput
 
     @classmethod
     def output(cls):
-        return Molecule
+        return Mol
 
     def execute(
         self,
@@ -89,63 +88,76 @@ class RDKitToMolComponent(GenericComponent):
         extra_outfiles: Optional[List[str]] = None,
         extra_commands: Optional[List[str]] = None,
         scratch_name: Optional[str] = None,
-        timeout: Optional[int] = None) -> Tuple[bool, Dict[str, Any]]:
-        
+        timeout: Optional[int] = None,
+    ) -> Tuple[bool, Dict[str, Any]]:
+
         if inputs.data:
             dtype = inputs.data.dtype
-            assert dtype == 'rdkit'
+            assert dtype == "rdkit"
             rdmol = inputs.data
-        elif inputs.code:
-            dtype = inputs.code.code_type.lower()
-            from mmelemental.models.molecule.rdkit_molecule import RDKitMolecule
-            rdmol = RDKitMolecule.build(inputs, dtype)            
-        elif inputs.file:
-            dtype = inputs.file.ext
-            from mmelemental.models.molecule.rdkit_molecule import RDKitMolecule
-            rdmol = RDKitMolecule.build(inputs, dtype)
+        elif inputs.code or inputs.file:
+            dtype = inputs.code.code_type.lower() if inputs.code else inputs.file.ext
+            from mmelemental.models.molecule.rdkit_mol import RDKitMol
+
+            rdmol = RDKitMol.build(inputs, dtype)
         else:
             raise NotImplementedError
-        
-        from mmelemental.models.molecule.rdkit_molecule import Bond
+
+        from mmelemental.models.molecule.rdkit_mol import Bond
 
         if inputs.args:
-            orient = inputs.args.get('orient')
-            validate = inputs.args.get('validate')
-            kwargs = inputs.args.get('kwargs')
+            orient = inputs.args.get("orient")
+            validate = inputs.args.get("validate")
+            kwargs = inputs.args.get("kwargs")
         else:
             orient, validate, kwargs = False, None, None
 
         symbs = [atom.GetSymbol() for atom in rdmol.mol.GetAtoms()]
 
         try:
-            residues = [(atom.GetPDBResidueInfo().GetResidueName(), 
-                        atom.GetPDBResidueInfo().GetResidueNumber()) 
-                        for atom in rdmol.mol.GetAtoms()]
-            names = [atom.GetPDBResidueInfo().GetName() for atom in rdmol.mol.GetAtoms()]
+            residues = [
+                (
+                    atom.GetPDBResidueInfo().GetResidueName(),
+                    atom.GetPDBResidueInfo().GetResidueNumber(),
+                )
+                for atom in rdmol.mol.GetAtoms()
+            ]
+            names = [
+                atom.GetPDBResidueInfo().GetName() for atom in rdmol.mol.GetAtoms()
+            ]
         except:
             residues = None
             names = None
-        
+
         connectivity = []
 
         for bond in rdmol.mol.GetBonds():
             bondOrder = Bond.orders.index(bond.GetBondType())
-            connectivity.append((bond.GetBeginAtomIdx(), bond.GetEndAtomIdx(), 1)) # replace 1 with bondOrder, 
+            connectivity.append(
+                (bond.GetBeginAtomIdx(), bond.GetEndAtomIdx(), 1)
+            )  # replace 1 with bondOrder,
             # for now this is a hack for qcelemental does not allow bond orders higher than 5
 
         # Get any random conformer?
         geo = rdmol.mol.GetConformer(0).GetPositions()
 
-        input_dict = {'symbols': symbs, 
-                      'geometry': geo, 
-                      'residues': residues, 
-                      'connectivity': connectivity,
-                      'names': names}        
+        input_dict = {
+            "symbols": symbs,
+            "geometry": geo,
+            "residues": residues,
+            "connectivity": connectivity,
+            "names": names,
+        }
 
         if kwargs:
             input_dict.update(kwargs)
 
         if inputs.code:
-            return True, Molecule(orient=orient, validate=validate, identifiers={dtype: inputs.code}, **input_dict)
+            return True, Mol(
+                orient=orient,
+                validate=validate,
+                identifiers={dtype: inputs.code},
+                **input_dict
+            )
         else:
-            return True, Molecule(orient=orient, validate=validate, **input_dict)
+            return True, Mol(orient=orient, validate=validate, **input_dict)
--- mmelemental/components/trans/template_component.py
+++ mmelemental/components/trans/template_component.py
@@ -2,6 +2,7 @@ from mmic.components.blueprints.generic_component import GenericComponent
 from mmelemental.util.decorators import require
 from typing import Dict, Any, List, Tuple, Optional
 
+
 class TransComponent(GenericComponent):
     """ A translator component template for converting between MMSchema and other MM codes. """
 
@@ -12,7 +13,7 @@ class TransComponent(GenericComponent):
 
     @staticmethod
     def has(obj: object, prop: str) -> bool:
-        """ Returns True if obj.prop exists and is not None. """ 
+        """ Returns True if obj.prop exists and is not None. """
         if hasattr(obj, prop):
             return False if getattr(obj, prop) is None else True
         else:
