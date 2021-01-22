611ba66aa47ec1e5390476adc2dd2a0dc19e1780
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-22 15:15:43 -0500

    renamed SimOutput to Traj

--- mmelemental/models/output/docking.py
+++ mmelemental/models/output/docking.py
@@ -1,17 +1,17 @@
 from typing import List, Optional
 from mmelemental.models.input.docking import DockingInput
-from mmelemental.models.output.sim import SimOutput
+from mmelemental.models.output.sim import Traj
 from mmelemental.models.base import Base
 from pydantic import Field
 
 
 class DockingOutput(Base):
     dockingInput: DockingInput = Field(..., description="Docking input model.")
-    ligand: SimOutput = Field(
+    ligand: Traj = Field(
         ...,
         description="Simulation output for the ligand, including its pose and score.",
     )
-    receptor: Optional[SimOutput] = Field(
+    receptor: Optional[Traj] = Field(
         None,
         description="Simulation output for non-rigid receptors i.e. conformation and orientation of the flexible side chains in the receptor relative to the ligand.",
     )
