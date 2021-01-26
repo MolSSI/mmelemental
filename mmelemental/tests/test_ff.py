46d8b15d24670425843e2518eb38183278fbaa86
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-25 22:42:24 -0500

    removed ParmEd component

--- mmelemental/tests/test_ff.py
+++ mmelemental/tests/test_ff.py
@@ -3,7 +3,6 @@ Forcefield tests for the mmelemental package.
 """
 import pytest
 from mmelemental.models.forcefield.io_ff import FFInput
-from mmelemental.components.trans.parmed_component import ParmedToFFComponent
 import sys
 
 
@@ -12,4 +11,4 @@ def test_mmelemental_imported():
     assert "mmelemental" in sys.modules
 
     ff_in = FFInput(file="mmelemental/data/molecules/alanine.top")
-    ff = ParmedToFFComponent.compute(ff_in)
+    #ff = ParmedToFFComponent.compute(ff_in)
