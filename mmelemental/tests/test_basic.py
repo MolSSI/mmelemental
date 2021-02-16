c0365d658be1a3396b103abf57e6b41c369a7488
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-16 00:42:42 -0500

    added test_basic.py

---
+++ mmelemental/tests/test_basic.py
@@ -0,0 +1,22 @@
+"""
+Basic sanity test for the mmelemental package.
+"""
+# import pytest
+import mmelemental
+from mmelemental.models.util.output import FileOutput
+from mmelemental.models.molecule.mm_mol import Molecule
+
+
+def test_mmelemental_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    import sys
+
+    assert "mmelemental" in sys.modules
+
+
+def test_mmelemental_mol_schema():
+    import os
+
+    mol = Molecule.from_file("mmelemental/data/molecules/alanine.json")
+    with FileOutput(path="mol.json", clean=True) as fo:
+        mol.to_file(fo.abs_path)
