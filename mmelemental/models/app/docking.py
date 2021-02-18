c273d64a41f9fdad22eb4c63ba88f59dd4d2913f
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-18 15:38:26 -0500

    refined docking schema; reverted back to lig/rec models

--- mmelemental/models/app/docking.py
+++ mmelemental/models/app/docking.py
@@ -3,13 +3,19 @@ from mmelemental.models.base import Base
 from mmelemental.models.molecule import Molecule
 from mmelemental.models.collect import Ensemble, Trajectory
 from pydantic import Field
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 
 
 class DockInput(SimInput):
-    searchSpace: Optional[
-        List[Tuple[float, float, float, float, float, float]]
-    ] = Field(
+    ligand: Union[List[Molecule], Molecule] = Field(
+        ...,
+        description="Ligand molecule object such as a drug. See the :class:``Molecule`` class. ",
+    )
+    receptor: Union[List[Molecule], Molecule] = Field(
+        ...,
+        description="Receptor molecule object such as a protein. See the :class:``Molecule`` class. ",
+    )
+    searchSpace: Optional[Tuple[float, float, float, float, float, float]] = Field(
         None,
         description="A 3D box defined by (xmin, xmax, ymin, ymax, zmin, zmax)."
         "The search space effectively restricts where the movable atoms, including those in the flexible side chains, should lie.",
@@ -18,12 +24,27 @@ class DockInput(SimInput):
 
 class DockOutput(Base):
     dockInput: DockInput = Field(..., description="Docking input model.")
-    ensemble: Ensemble = Field(
-        ...,
-        description="Ensemble output for the ligand pose and score, and optionally the (flexible) receptor.",
+    ligands: List[Molecule] = Field(
+        ...,                                                                                                                                
+        description="Molecule object(s) representing ligands. See the :class:``Molecule`` class.",
     )
-
-
-class AffinityOutput(Base):
-    dockOutput: DockOutput
-    affinity: float
+    receptors: Optional[List[Molecule]] = Field(
+        None,
+        description="Molecule object(s) representing flexible receptors e.g. side-chains. See the :class:``Molecule`` class.",
+    )
+    scores: List[float] = Field(
+        ...,
+        description="Scores for each pose. Typically the score is the binding affinity. Length must be equal to the number of poses.",
+    )
+    scores_units: Optional[str] = Field(
+        "kJ/mol",
+        description="Score function unit."
+    )
+    observables: Optional[Dict[str, List[float]]] = Field(
+        None,
+        description="Physical observables for each pose. E.g. observables={'RMSD':[...]}."
+    )
+    observables_units: Optional[Dict[str,str]] = Field(
+        None,
+        description="Physical observables units. E.g. observables_units={'RMSD':'angstrom'}."
+    )
