40424913926a1523ce76c76149b82a3edcf1115b
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-19 22:24:06 -0500

    replaced FFInput with ForceField

--- mmelemental/tests/test_ff.py
+++ mmelemental/tests/test_ff.py
@@ -2,13 +2,10 @@
 Forcefield tests for the mmelemental package.
 """
 import pytest
-from mmelemental.models.forcefield.io_ff import FFInput
+from mmelemental.models.forcefield import ForceField
 import sys
 
 
 def test_mmelemental_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmelemental" in sys.modules
-
-    ff_in = FFInput(file="mmelemental/data/molecules/alanine.top")
-    # ff = ParmedToFFComponent.compute(ff_in)
