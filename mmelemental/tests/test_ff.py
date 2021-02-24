71d6ae1fd714a9db320868660107bef0c65b1139
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-24 18:00:24 -0500

    added simple test function for gmx

--- mmelemental/tests/test_ff.py
+++ mmelemental/tests/test_ff.py
@@ -4,8 +4,18 @@ Forcefield tests for the mmelemental package.
 import pytest
 from mmelemental.models.forcefield import ForceField
 import sys
+import os
+
+data_dir = os.path.join("mmelemental", "data", "molecules")
 
 
 def test_mmelemental_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmelemental" in sys.modules
+
+
+def test_mmelemental_read_gmx():
+    groFile = os.path.join(data_dir, "alanine.top")
+
+    mm_ff = ForceField.from_file(groFile)
+    assert isinstance(mm_ff, ForceField)
