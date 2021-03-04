4b51f124449c584bef76396ae0ce496d10cf1af6
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-04 15:03:11 -0500

    create ff_trans

---
+++ mmelemental/tests/test_ff_trans.py
@@ -0,0 +1,32 @@
+"""
+Forcefield tests for the mmelemental package.
+"""
+import pytest
+from mmelemental.models.forcefield import ForceField
+import sys
+import os
+from .data import data_dir
+
+try:
+    import mmic_translator
+
+    translators = mmic_translator.components.TransComponent.installed()
+except Exception:
+    translators = []
+
+
+def pytest_generate_tests(metafunc):
+    if "translator" in metafunc.fixturenames:
+        metafunc.parametrize("translator", translators)
+
+
+def test_mmelemental_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "mmelemental" in sys.modules
+
+
+def test_mmelemental_moldata(translator):
+    topFile = os.path.join(data_dir, "alanine.top")
+
+    mm_ff = ForceField.from_file(topFile, translator=translator)
+    assert isinstance(mm_ff, ForceField)
