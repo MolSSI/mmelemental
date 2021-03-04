c21492ec5d07f89086c18deb4ae2a18b9096e3d8
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-04 15:03:34 -0500

    testing works even when no trans is available

--- mmelemental/tests/test_mol_trans.py
+++ mmelemental/tests/test_mol_trans.py
@@ -4,31 +4,29 @@ Unit and regression test for the mmelemental package.
 import pytest
 import sys
 import os
-import parmed
 import mmelemental
-import mmic_translator
 from mmelemental.models.util.input import FileInput
 from mmelemental.models.molecule.mm_mol import Molecule
-from mmelemental.models.chem.codes import ChemCode
+
 from mmelemental.models.molecule.io_mol import MolInput, MolOutput
 from mmelemental.components.io.constructor_component import (
     MolConstructorComponent,
     ForceFieldConstructorComponent,
 )
 
+from .data import data_dir
 
-data_dir = os.path.join("mmelemental", "data", "molecules")
+try:
+    import mmic_translator
 
-translators = mmic_translator.components.TransComponent.installed()
+    translators = mmic_translator.components.TransComponent.installed()
+except Exception:
+    translators = []
+
 
 def pytest_generate_tests(metafunc):
     if "translator" in metafunc.fixturenames:
         metafunc.parametrize("translator", translators)
-
-
-def test_mmelemental_imported():
-    """Sample test, will always pass so long as import statement worked"""
-    assert "mmelemental" in sys.modules
 
 
 def test_mmelemental_moldata(translator):
@@ -44,18 +42,6 @@ def test_mmelemental_moltop(translator):
     mm_mol = Molecule.from_file(groFile, topFile, translator=translator)
     assert mm_mol.connectivity is not None
 
-@pytest.mark.skip(reason="Need rdkit installed to handle codes for now.")
-def test_mmelemental_codes():
-    smiles = ChemCode(code="CCCC")
-    inputs = MolInput(code=smiles)
-    return MolConstructorComponent.compute(inputs)
-
-
-def test_mmelemental_json():
-    jsonFile = os.path.join(data_dir, "alanine.json")
-    mm_mol = Molecule.from_file(jsonFile)
-    assert isinstance(mm_mol, Molecule)
-
 
 def test_mmelemental_mol_tofile(translator):
     for ext in ["pdb", "gro"]:
@@ -64,7 +50,7 @@ def test_mmelemental_mol_tofile(translator):
         mol = Molecule.from_file(pdbFile, translator=translator)
 
         mol.to_file("mol.pdb")
-        mol.to_file("mol.json") #, indent=2)
+        mol.to_file("mol.json")  # , indent=2)
         # mol.to_file("mol.gro") -> broken in mmic_parmed, why?!
         # mol.to_file("rdkit.xyz")
         # mol.to_file('rdkit.smiles')
