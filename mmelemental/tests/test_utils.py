b48909ac1f707df71f24188016a97a156702d543
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-04 15:04:26 -0500

    use data files from data.py

--- mmelemental/tests/test_utils.py
+++ mmelemental/tests/test_utils.py
@@ -6,18 +6,20 @@ from mmelemental.models.util.input import FileInput, OpenBabelInput, GrepInput
 from mmelemental.models.chem.codes import ChemCode
 
 # Test input file
-receptor = FileInput(path="mmelemental/data/molecules/alanine.pdb")
+from .data import data_dir
+
+receptor = os.path.join(data_dir, "alanine.pdb")
 
 
 def test_obabel():
     from mmelemental.components.util.openbabel_component import OpenBabelComponent
 
-    obabel_input = OpenBabelInput(fileInput=receptor, outputExt="pdbqt")
+    obabel_input = OpenBabelInput(fileInput=FileInput(path=receptor), outputExt="pdbqt")
     obabel_output = OpenBabelComponent.compute(obabel_input)
 
 
 def test_grep():
     from mmelemental.components.util.grep_component import GrepComponent
 
-    grep_input = GrepInput(fileInput=receptor, pattern="ATOM")
+    grep_input = GrepInput(fileInput=FileInput(path=receptor), pattern="ATOM")
     grep_output = GrepComponent.compute(grep_input)
