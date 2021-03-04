ab4d09e316f3ad065716a0640a25df088666284c
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-04 14:23:16 -0500

    add name validation

--- mmelemental/models/forcefield/nonbonded/nb_params.py
+++ mmelemental/models/forcefield/nonbonded/nb_params.py
@@ -1,4 +1,6 @@
 from mmelemental.models.forcefield.params import Params
+from pydantic import Field, validator
+from typing import Optional
 import os
 import pathlib
 
@@ -9,3 +11,15 @@ class NonBondedParams(Params):
     _path_name = os.path.join(
         pathlib.Path(__file__).parent.absolute(), "potentials", "*.py"
     )
+    name: Optional[str] = Field(None, description="Name or form of the potential.")
+    _path_name = os.path.join(
+        pathlib.Path(__file__).parent.absolute(), "potentials", "*.py"
+    )
+
+    # Validators
+    
+    @validator("name", always=True)
+    def _set_name(cls, v, values):
+        if v is not None:
+            assert v == cls.__name__
+        return cls.__name__
