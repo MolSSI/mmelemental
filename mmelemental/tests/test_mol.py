d28e144a0707072bb6018279abb2263574e9740d
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-04 15:03:50 -0500

    create mol test with no deps

---
+++ mmelemental/tests/test_mol.py
@@ -0,0 +1,18 @@
+import pytest
+from mmelemental.models.chem.codes import ChemCode
+from mmelemental.models.molecule.mm_mol import Molecule
+import os
+from .data import data_dir
+
+
+@pytest.mark.skip(reason="Need rdkit installed to handle codes for now.")
+def test_mmelemental_codes():
+    smiles = ChemCode(code="CCCC")
+    inputs = MolInput(code=smiles)
+    return MolConstructorComponent.compute(inputs)
+
+
+def test_mmelemental_json():
+    jsonFile = os.path.join(data_dir, "alanine.json")
+    mm_mol = Molecule.from_file(jsonFile)
+    assert isinstance(mm_mol, Molecule)
