222038de0ef62f68320aa7d7db9f037da2379a3a
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-17 05:24:23 -0400

    add test_ff_read

---
+++ mmelemental/tests/test_ff_read.py
@@ -0,0 +1,15 @@
+from mmelemental.models import forcefield as ff
+import glob
+import os
+from .data import data_ff_dir
+
+json_files = glob.glob(os.path.join(data_ff_dir, "*.json"))
+
+
+def pytest_generate_tests(metafunc):
+    if "json_file" in metafunc.fixturenames:
+        metafunc.parametrize("json_file", json_files)
+
+
+def test_read_json(json_file):
+    ff.ForceField.from_file(json_file)
