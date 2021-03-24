aa464bdaf7a6a0a2545d0f56c3ab39365ef901bb
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-24 12:04:03 -0400

    import util

--- mmelemental/models/__init__.py
+++ mmelemental/models/__init__.py
@@ -1,5 +1,7 @@
-from . import collect, chem, molecule, proc, solvent, util, forcefield
+from . import collect, chem, molecule, proc, solvent, util, forcefield, base
 from .molecule import *
 from .forcefield import *
 from .collect import *
 from .proc import *
+from .base import *
+from .util import *
