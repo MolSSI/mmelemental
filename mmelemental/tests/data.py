7f628b0fb978080ed0ce489fddde52beecc167d6
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-09 16:58:21 -0400

    delete data.py

--- mmelemental/tests/data.py
+++
@@ -1,6 +0,0 @@
-import os
-
-__all__ = ["data_mol_dir", "data_ff_dir"]
-
-data_mol_dir = os.path.join("mmelemental", "data", "molecules")
-data_ff_dir = os.path.join("mmelemental", "data", "forcefields")
