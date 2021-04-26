295b2327dcc56ebe716e1fed2ce3047883477fa4
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-26 06:58:03 -0400

    automate FF args for pytest

--- mmelemental/tests/test_ff_write.py
+++ mmelemental/tests/test_ff_write.py
@@ -8,6 +8,41 @@ nbonds = natoms - 1
 nangles = natoms - 2
 ndihedrals = natoms - 3
 
+# Data
+rand_npts = numpy.random.rand(natoms)
+rand_nbond = numpy.random.rand(nbonds)
+rand_nangle = numpy.random.rand(nangles)
+rand_ndih = numpy.random.rand(ndihedrals)
+rand_int_ndih = numpy.random.randint(ndihedrals, size=ndihedrals)
+
+# Pytest parametrization args
+nb_pytest_params = "form,params", [
+    ("LennardJones", {"epsilon": rand_npts, "sigma": rand_npts})
+]
+bonds_pytest_params = "form,lengths,params", [
+    ("Harmonic", rand_nbond, {"spring": rand_nbond}),
+    ("Gromos96", rand_nbond, {"spring": rand_nbond}),
+]
+angles_pytest_params = "form,lengths,params", [
+    ("Harmonic", rand_nangle, {"spring": rand_nangle})
+]
+dihedrals_pytest_params = "form,lengths,params", [
+    (
+        "Charmm",
+        rand_ndih,
+        {"energy": rand_ndih, "periodicity": rand_int_ndih, "phase": rand_ndih},
+    ),
+    (
+        "CharmmMulti",
+        rand_ndih,
+        {
+            "energy": (rand_ndih, rand_ndih),
+            "periodicity": (rand_int_ndih, rand_int_ndih),
+            "phase": (rand_ndih, rand_ndih),
+        },
+    ),
+]
+
 
 def rewrite(filename):
     import json
@@ -20,91 +55,81 @@ def rewrite(filename):
 
 
 def rand_angles(npts):
-    return numpy.random.rand(npts) * numpy.pi
+    return rand_data * numpy.pi
 
 
 ###########################################
 ####### NON-BONDED ########################
-def test_nonbonded():
-    lj = ff.nonbonded.potentials.LennardJones(
-        epsilon=numpy.random.rand(natoms), sigma=numpy.random.rand(natoms)
-    )
-    nonbonded = ff.nonbonded.NonBonded(params=lj, form="LennardJones")
-    assert nonbonded.form == "LennardJones"
+@pytest.mark.parametrize(*nb_pytest_params)
+def test_nonbonded(form, params):
+    potential = getattr(ff.nonbonded.potentials, form)(**params)
+    nonbonded = ff.nonbonded.NonBonded(params=potential, form=form)
+    assert nonbonded.form == form
     return nonbonded
 
 
 ############################################
 ######## BONDS #############################
-def test_bonds_linear():
+@pytest.mark.parametrize(*bonds_pytest_params)
+def test_bonds(form, lengths, params):
     indices = [(i, i + 1, 1.0) for i in range(nbonds)]
-    linear = ff.bonded.bonds.potentials.Harmonic(spring=numpy.random.rand(nbonds))
+    potential = getattr(ff.bonded.bonds.potentials, form)(**params)
     bonds = ff.bonded.Bonds(
-        params=linear,
-        lengths=numpy.random.rand(nbonds),
-        form="Harmonic",
+        params=potential,
+        lengths=lengths,
+        form=form,
         indices=indices,
     )
-    assert bonds.form == "Harmonic"
-    return bonds
-
-
-def test_bonds_gromos96():
-    indices = [(i, i + 1, 1.0) for i in range(nbonds)]
-    lengths = numpy.random.rand(nbonds)
-    gromos = ff.bonded.bonds.potentials.Gromos96(
-        spring=numpy.random.rand(nbonds),
-    )
-
-    bonds = ff.bonded.Bonds(
-        params=gromos, lengths=lengths, indices=indices, form="Gromos96"
-    )
-    assert bonds.form == "Gromos96"
+    assert bonds.form == form
     return bonds
 
 
 ###########################################
 ####### ANGLES ############################
-def test_angles():
+@pytest.mark.parametrize(*angles_pytest_params)
+def test_angles(form, lengths, params):
     indices = [(i, i + 1, i + 2) for i in range(nangles)]
-    linear = ff.bonded.angles.potentials.Harmonic(
-        spring=numpy.random.rand(nangles),
-    )
+    potential = getattr(ff.bonded.angles.potentials, form)(**params)
     angles = ff.bonded.Angles(
-        params=linear,
-        angles=rand_angles(nangles),
+        params=potential,
+        angles=lengths,
         angles_units="radians",
         indices=indices,
-        form="Harmonic",
+        form=form,
     )
-    assert angles.form == "Harmonic"
+    assert angles.form == form
     return angles
 
 
 ###########################################
 ####### DIHEDRALS #########################
-def test_dihedrals():
+@pytest.mark.parametrize(*dihedrals_pytest_params)
+def test_dihedrals(form, lengths, params):
     indices = [(i, i + 1, i + 2, i + 3) for i in range(ndihedrals)]
-    linear = ff.bonded.dihedrals.potentials.Harmonic(
-        energy=numpy.random.rand(ndihedrals),
-        periodicity=numpy.random.randint(ndihedrals),
-    )
+    potential = getattr(ff.bonded.dihedrals.potentials, form)(**params)
     dihedrals = ff.bonded.Dihedrals(
-        params=linear,
-        angles=rand_angles(ndihedrals),
-        angles_units="radians",
+        params=potential,
+        # angles=lengths,
+        # angles_units="radians",
         indices=indices,
-        form="Harmonic",
+        form=form,
     )
-    assert dihedrals.form == "Harmonic"
+    assert dihedrals.form == form
     return dihedrals
 
 
-def test_forcefield():
-    nonbonded = test_nonbonded()
-    bonds = test_bonds_linear()
-    angles = test_angles()
-    dihedrals = test_dihedrals()
+@pytest.mark.parametrize(*dihedrals_pytest_params)
+def test_forcefield(form, lengths, params):
+    nonbonded = test_nonbonded(
+        form="LennardJones", params={"epsilon": rand_npts, "sigma": rand_npts}
+    )
+    bonds = test_bonds(
+        form="Harmonic", lengths=rand_nbond, params={"spring": rand_nbond}
+    )
+    angles = test_angles(
+        form="Harmonic", lengths=rand_nangle, params={"spring": rand_nangle}
+    )
+    dihedrals = test_dihedrals(form, lengths, params)
     mm_ff = ff.ForceField(
         nonbonded=nonbonded,
         bonds=bonds,
@@ -118,6 +143,7 @@ def test_forcefield():
     os.remove("forcefield-single.json")
 
 
+@pytest.mark.skip("Cannot run this now")
 def test_forcefield_hybrid():
     nonbonded = test_nonbonded()
     bonds = [test_bonds_linear(), test_bonds_gromos96()]
