d328f2f132c1e35b9dc252612dff2b5c47305dbd
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-07-20 15:09:31 -0400

    replace qcel with cmsel

--- mmelemental/models/collect/sm_ensem.py
+++ mmelemental/models/collect/sm_ensem.py
@@ -1,6 +1,6 @@
 from pydantic import Field, constr
 from typing import Optional, List, Dict, Any
-from qcelemental.models.types import Array
+from cmselemental.types import Array
 from mmelemental.models.molecule.mm_mol import Molecule
 from mmelemental.models.base import ProtoModel, Provenance, provenance_stamp
 import functools
