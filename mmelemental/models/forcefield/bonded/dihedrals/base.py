e6f4cdcc3db84bf6e97b22d276b32cbd8eac7e56
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-07-21 10:00:12 -0400

    uncomment code

--- mmelemental/models/forcefield/bonded/dihedrals/base.py
+++ mmelemental/models/forcefield/bonded/dihedrals/base.py
@@ -1,6 +1,6 @@
 from pydantic import Field, validator
 from mmelemental.models.forcefield.params import Params
-import qcelemental
+from cmselemental.types import Array
 from typing import Optional, List, Tuple
 import os
 import pathlib
@@ -10,24 +10,24 @@ __all__ = ["Dihedrals"]
 
 class Dihedrals(Params):
     _path = os.path.join(pathlib.Path(__file__).parent.absolute(), "potentials", "*.py")
-    # angles: qcelemental.models.types.Array[float] = Field(
-    #    None, description="Equilibrium dihedral angles. Default unit is degrees."
-    # )
-    # angles_units: Optional[str] = Field(
-    #    "degrees", description="Equilibrium dihedral angle units."
-    # )
+    angles: Array[float] = Field(
+        None, description="Equilibrium dihedral angles. Default unit is degrees."
+    )
+    angles_units: Optional[str] = Field(
+        "degrees", description="Equilibrium dihedral angle units."
+    )
     indices: List[Tuple[int, int, int, int]] = Field(  # type: ignore
         ...,
         description="Particle indices for each dihedral angle.",
         min_items=1,
     )
-    weights: Optional[qcelemental.models.types.Array[float]] = Field(
+    weights: Optional[Array[float]] = Field(
         None,
         description="Something to consider later on? Ses CHARMM dihedral_style for LAMMPS.",
     )
 
     # Validators
-    # @validator("angles")
+    @validator("angles")
     def _angles_shape(cls, v, values):
         assert len(v.shape) == 1, "Bond lengths must be a 1D array!"
         return v
