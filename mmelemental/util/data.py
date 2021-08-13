dcb42d530fde73cba33fd332d6f65b7012ca3d2f
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-08-13 15:43:14 -0400

    create data submodule

---
+++ mmelemental/util/data.py
@@ -0,0 +1,62 @@
+import numpy
+from typing import Any, Union
+
+
+numpy_int = "i8"
+numpy_float = "f8"
+numpy_unicode = "U4"
+
+
+def float_prep(array, around):
+    """
+    Rounds floats to a common value and build positive zeros to prevent hash conflicts.
+    """
+    if isinstance(array, (list, numpy.ndarray)):
+        # Round array
+        array = numpy.around(array, around)
+        # Flip zeros
+        array[numpy.abs(array) < 5 ** (-(around + 1))] = 0
+
+    elif isinstance(array, (float, int)):
+        array = round(array, around)
+        if array == -0.0:
+            array = 0.0
+    else:
+        raise TypeError("Type '{}' not recognized".format(type(array).__name__))
+
+    return array
+
+
+def get_dtype(data: Any) -> Union[str, None]:
+    """Returns a numpy data type for an object. Integers and floats are represented in 8 bytes (64 bits)
+    precision while strings in 4 characters (128 bits) UTF-8 unicode.
+
+    Returns
+    -------
+    str or None
+        Returns data type if found otherwise None.
+
+    Raises
+    ------
+    ValueError
+        When `data` type is not a str, float, int, list, tuple, or numpy.ndarray.
+    """
+    if isinstance(data, str):
+        return numpy_unicode
+    elif isinstance(data, float):
+        return numpy_float
+    elif isinstance(data, int):  # check for more types?
+        return numpy_int
+    elif isinstance(data, (list, tuple, numpy.ndarray)):
+        if isinstance(data[0], (list, tuple, numpy.ndarray)):
+            if isinstance(data[0], numpy.ndarray):
+                if data.dtype != object:
+                    return None  # homogenous array
+            dtype = ",".join([get_dtype(item) for item in data[0]])
+        elif all([isinstance(item, str) for item in data]):
+            dtype = str
+        else:
+            return None  # We assume homogenous array in MMSchema esle dtype=object
+        return dtype
+    else:
+        raise ValueError(f"Data type {type(data)} not supported.")
