e6fdcb9a1a3d3e0bd520e3966281bd87dec78e10
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-08-27 13:11:21 -0400

    get logo from _static

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -109,7 +109,7 @@ external_urls = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-html_logo = "logos/asteroid_logo_dark_allblack.png"
+html_logo = "_static/mmel_logo.png"
 
 html_theme_options = {
     "logo_only": True,
@@ -127,7 +127,7 @@ html_sidebars = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
