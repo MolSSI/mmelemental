e46282c445c3e75672feb8c08d237926e2db8687
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-13 15:30:50 -0400

    add combination rules

--- mmelemental/models/forcefield/nonbonded/base.py
+++ mmelemental/models/forcefield/nonbonded/base.py
@@ -1,4 +1,6 @@
 from mmelemental.models.forcefield.params import Params
+from pydantic import Field
+from typing import Optional
 import os
 import pathlib
 
@@ -9,3 +11,6 @@ class NonBonded(Params):
     """Model that describes non-bonded interactions between particles."""
 
     _path = os.path.join(pathlib.Path(__file__).parent.absolute(), "potentials", "*.py")
+    combination_rule: Optional[str] = Field(
+        "Lorentz-Berthelot", description="Combination rule for the force field."
+    )
