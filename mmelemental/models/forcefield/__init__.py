9b378744532e489a63f61a1da47650af4d59a7f2
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-21 15:56:22 -0400

    update import path

--- mmelemental/models/forcefield/__init__.py
+++ mmelemental/models/forcefield/__init__.py
@@ -1,9 +1,9 @@
-from .mm_ff import *
+from .forcefield import *
 from .nonbonded import *
 from .bonded import *
 
 from . import nonbonded
 from . import bonded
-from . import mm_ff
+from . import forcefield
 
-__all__ = mm_ff.__all__ + nonbonded.__all__ + bonded.__all__
+__all__ = forcefield.__all__ + nonbonded.__all__ + bonded.__all__
