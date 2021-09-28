e4a44ff09843639c984ea888506c2d987cd29703
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-28 00:29:41 -0400

    import improper dihedrals

--- mmelemental/models/forcefield/bonded/__init__.py
+++ mmelemental/models/forcefield/bonded/__init__.py
@@ -1,7 +1,10 @@
 from .bonds import *
 from .angles import *
 from .dihedrals import *
+from .dihedrals_improper import *
 
-from . import bonds, angles, dihedrals
+from . import bonds, angles, dihedrals, dihedrals_improper
 
-__all__ = bonds.__all__ + angles.__all__ + dihedrals.__all__
+__all__ = (
+    bonds.__all__ + angles.__all__ + dihedrals.__all__ + dihedrals_improper.__all__
+)
