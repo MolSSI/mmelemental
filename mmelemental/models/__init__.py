aae1cda1bc92a19ef74ad00cb7481ad72b46cf4d
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-10-05 17:03:43 -0400

    remove chem import

--- mmelemental/models/__init__.py
+++ mmelemental/models/__init__.py
@@ -1,4 +1,4 @@
-from . import collect, chem, struct, util, forcefield, base
+from . import collect, struct, util, forcefield, base
 from .struct import Molecule
 from .forcefield import ForceField
 from .collect import Trajectory, Ensemble
